837f1c0f7a689f45a5de1ec6eaca7fff1de3ee3b
gavraluca <glm21@yahoo.com>
2026-03-04 11:33:26 +0200

    Uploaded default current_data.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ current_data = {
     "temp" : 25.0,
     "humidity" : 44.0,
     "pressure" : 1010.0,
-    "gas-lvl" : 400
+    "gas_lvl" : 400
 }
+
+# ESP32 sends data via WI-FI:
+@app.route('/api/update', methods=['POST'])
+def update_data():
+    global current_data
+    data = request.get_json()
+
+    if data:
+        current_data.update(data)
+        print(f"New data --- Temp: {current_data['temp']}°C --- Gas: {current_data['gas_lvl']}")
+        return jsonify({"Confirmation " : " SUCCES" , "Message " : " Data saved!"}), 200
+    return jsonify({"Confirmation " : " ERROR" , "Message " : " INVALID DATA!"}), 400
 
